bd948070eb5cf1afea879f85aaf72967a62ee3b6
Carlos Samuel <carlosamuel@alu.ufc.br>
2024-05-30 10:30:13 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,42 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <TheHeader v-if="showHeader"/>
+
+  <div v-show="showName">
+    Nome: {{ firstnName }} <br>
+    Nome: {{ lastName }}
+  </div>
+
+  <div v-if="acessLevel === 'admin'"> Admin </div>
+  <div v-else-if="acessLevel === 'marketing'"> Marketing </div>
+  <div v-else >Usuario norma</div>
+
+  <img  
+    alt="Vue logo" src="./assets/logo.png">
+  
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, TheHeader
+  },
+  data() {
+    return {
+      showHeader: false,
+      firstnName: 'Jon',
+      lastName: 'Snow',
+      showName: false,
+      acessLevel: 'marketing'
+    
+    }
   }
+
 }
 </script>
 
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,20 @@
+<template>
+    <header class="header">
+        Header
+    </header>
+</template>
+
+<script>
+    export default {
+    
+    }
+</script>
+
+<style>
+    .header {
+        background-color: #333;
+        color: white;
+        padding: 10px;
+        text-align: center;
+    }
+</style>
