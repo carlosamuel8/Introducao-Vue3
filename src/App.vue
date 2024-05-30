c9ddb73e88a0b85c8abfbed4fc07df8d5af5113f
Carlos Samuel <carlosamuel@alu.ufc.br>
2024-05-30 18:48:58 -0300

    teste

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,77 @@
 <template>
   <TheHeader v-if="showHeader"/>
 
-  <div v-show="showName">
+  <!-- <div v-show="showName">
     Nome: {{ firstnName }} <br>
     Nome: {{ lastName }}
   </div>
 
   <div v-if="acessLevel === 'admin'"> Admin </div>
   <div v-else-if="acessLevel === 'marketing'"> Marketing </div>
-  <div v-else >Usuario norma</div>
-
+  <div v-else >Usuario norma</div> -->
+<!-- 
   <img  
     alt="Vue logo" src="./assets/logo.png">
   
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   
+  <h1 :class="classvar" ></h1>
+
+
+  <div 
+    v-for="(obj, index) in todos" 
+    :key="obj.id" 
+    class="todo-item"
+    >
+     {{index}} - {{ obj.title }}
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheHeader from './components/TheHeader.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheHeader from './components/TheHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld, TheHeader
+    // HelloWorld, TheHeader
   },
   data() {
     return {
-      showHeader: false,
-      firstnName: 'Jon',
-      lastName: 'Snow',
-      showName: false,
-      acessLevel: 'marketing'
-    
+      classvar: 'title',
+      todos: [
+        {
+          "userId": 1,
+          "id": 1,
+          "title": "delectus aut autem",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 2,
+          "title": "quis ut nam facilis et officia qui",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 3,
+          "title": "fugiat veniam minus",
+          "completed": false
+        },
+        {
+          "userId": 1,
+          "id": 4,
+          "title": "et porro tempora",
+          "completed": true
+        },
+        {
+          "userId": 1,
+          "id": 5,
+          "title": "laboriosam mollitia et enim quasi adipisci quia provident illum",
+          "completed": false
+        }
+        ]    
     }
   }
 
@@ -45,8 +83,22 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px;
 }
+
+.todo-item {
+  border: 1px solid #ccc;
+  margin: 10px;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.title{
+  color: red;
+  font-size: large;
+}
+
+
 </style>
